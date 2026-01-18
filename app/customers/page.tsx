e5a5468761ba3d63a31cffbd292039b5563fb313
Satyam Sagar <satyamsagar30@gmail.com>
2026-01-18 16:37:57 +0530

    refactor file structure

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -1,4 +1,4 @@
-export default function UsersPage() {
+export default function CustomersPage() {
   return (
     <div className="grid auto-rows-min gap-4 md:grid-cols-4">
       <div className="bg-muted/50 aspect-video rounded-xl" />
